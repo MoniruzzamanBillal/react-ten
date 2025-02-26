598d6d6f9e810501511321e49ce999b5f60333ed
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-02-26 15:06:03 +0600

    initialize

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="mainContainer">
+      <h1>main layout </h1>
+      <h1>main layout </h1>
+      <h1>main layout </h1>
+      <h1>main layout </h1>
+    </div>
+  );
+}
+
+export default App;
